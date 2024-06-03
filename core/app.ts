3c6551c795b9dad4bf0dbf9f17f8d3dc389d5547
agam-w <agam.w11@gmail.com>
2024-06-03 17:53:20 +0700

    add cors

--- core/app.ts
+++ core/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import userRoutes from "../routes/userRoutes";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 // json body parser middleware
 app.use(express.json());
+// cors middleware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
