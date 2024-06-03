58820fc31bccf5dfa67357ab47b1e9b0833ab563
agam-w <agam.w11@gmail.com>
2024-06-03 12:53:07 +0700

    api error entity

--- repository/userCollection.ts
+++ repository/userCollection.ts
@@ -1,16 +1,37 @@
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, getDocs } from "firebase/firestore";
+import { FirestoreError } from "firebase/firestore";
 import { db } from "../config/firebaseConfig";
+import { ApiError } from "../entities/ApiError";
 
+/**
+ * get list of users from collection
+ */
 export const getUsers = async () => {
-  const q = query(collection(db, "USERS"));
-  const querySnapshot = await getDocs(q);
+  try {
+    const q = query(collection(db, "USERS"));
+    const querySnapshot = await getDocs(q);
 
-  const users = querySnapshot.docs.map((doc) => {
-    return {
-      id: doc.id,
-      ...doc.data(),
-    };
-  });
+    const users = querySnapshot.docs.map((doc) => {
+      return {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
 
-  return users;
+    return users;
+  } catch (error) {
+    if (error instanceof FirestoreError) {
+      console.error("Firestore Error:", error.code, error.message);
+      return {
+        code: error.code,
+        message: error.message,
+      } as ApiError;
+    } else {
+      console.error("Unexpected Error:", error);
+      return {
+        code: "unexpected",
+        message: "Unexpected Error",
+      } as ApiError;
+    }
+  }
 };
